ff3276d1523f5ad3608a7b1ff7ce825301a53ca3
Dublin R <DublinRUserGroup@gmail.com>
2013-12-17 19:36:35 +0000

    Update SurvivalModels.R

--- SurvivalModels.R
+++ SurvivalModels.R
@@ -18,3 +18,11 @@ summary(my.fit)$n.risk # {Y_i}
 summary(my.fit)$n.event # {d_i}
 summary(my.fit)$std.err # standard error of the K-M estimate at {t_i}
 summary(my.fit)$lower # lower pointwise estimates (alternatively, $upper)
+
+#######################################################
+
+lifetimes <- rexp( 25, rate = 0.2)
+censtimes <- 5 + 5*runif(25)
+ztimes <- pmin(lifetimes, censtimes)
+status <- as.numeric(censtime > lifetimes)
+
