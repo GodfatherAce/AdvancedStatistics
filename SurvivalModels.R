4cdfe24210fb6c01a7f07dc4ea9c0572a819d4f4
Dublin R <DublinRUserGroup@gmail.com>
2013-12-17 19:33:34 +0000

    Create SurvivalModels.R

---
+++ SurvivalModels.R
@@ -0,0 +1,20 @@
+# http://stat.ethz.ch/education/semesters/ss2011/seminar/diez.pdf
+install.packages(’survival’)
+install.packages(’KMsurv’)
+
+library(survival)
+library(KMsurv)
+
+##############################################
+data(tongue); attach(tongue)
+my.surv <- Surv(time[type==1], delta[type==1])
+survfit(my.surv)
+
+######################################################
+my.fit <- survfit(my.surv)
+summary(my.fit)$surv # outputs the Kaplan-Meier estimate at each t_i
+summary(my.fit)$time # {t_i}
+summary(my.fit)$n.risk # {Y_i}
+summary(my.fit)$n.event # {d_i}
+summary(my.fit)$std.err # standard error of the K-M estimate at {t_i}
+summary(my.fit)$lower # lower pointwise estimates (alternatively, $upper)
